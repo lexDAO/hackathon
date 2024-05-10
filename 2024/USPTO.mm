11d06e6f42e256be41531d792a67994b406c61a5
Dr Lawrence Lau <14944510+drllau@users.noreply.github.com>
2024-05-11 06:46:28 +0800

    Mindmap of USPTO refusal and lines of argument

---
+++ 2024/USPTO.mm
@@ -0,0 +1,179 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="USPTO" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1714372055068" STYLE="oval" NodeVisibilityConfiguration="SHOW_HIDDEN_NODES">
+<font SIZE="13" BOLD="true"/>
+<hook NAME="MapStyle" zoom="1.5">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/essay.mm" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1190209345" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1190209345" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.ok">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.needs_action">
+<icon BUILTIN="messagebox_warning"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.floating_node">
+<cloud COLOR="#ffffff" SHAPE="ARC"/>
+<edge STYLE="hide_edge"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork">
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_75090151" COLOR="#ff0000">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#ff3333" TRANSPARENCY="255" DESTINATION="ID_75090151"/>
+<font NAME="Liberation Sans" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.question">
+<icon BUILTIN="help"/>
+<font NAME="Aharoni" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.key" COLOR="#996600">
+<icon BUILTIN="password"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.idea">
+<icon BUILTIN="idea"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.note" COLOR="#990000">
+<font NAME="Liberation Sans" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.date" COLOR="#0033ff">
+<icon BUILTIN="calendar"/>
+<font NAME="Liberation Sans" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.website" COLOR="#006633">
+<font NAME="Liberation Sans" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.list" COLOR="#cc6600">
+<icon BUILTIN="list"/>
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.quotation" COLOR="#338800" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.definition" COLOR="#666600">
+<font NAME="Liberation Sans" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.description" COLOR="#996600">
+<font NAME="Liberation Sans" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.pending" COLOR="#b3b95c">
+<font NAME="Liberation Sans" SIZE="10"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="14" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="refutal" POSITION="right" ID="ID_1823382018" CREATED="1712402680250" MODIFIED="1714371843898">
+<edge COLOR="#00007c"/>
+<font BOLD="true"/>
+<node TEXT="DAO (UNA - DUNA)" ID="ID_1431924649" CREATED="1712402762409" MODIFIED="1714371843892" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="0.75 pt">
+<edge COLOR="#00007c"/>
+<node TEXT="germane to purpose" ID="ID_259900094" CREATED="1712402876997" MODIFIED="1714371834437">
+<edge COLOR="#003399"/>
+</node>
+<node TEXT="concurrent use" ID="ID_1155242076" CREATED="1712403450338" MODIFIED="1714371736866"/>
+<node TEXT="legitimate control" ID="ID_1385060510" CREATED="1712403713374" MODIFIED="1712403717525"/>
+<node TEXT="not certification" ID="ID_48808182" CREATED="1712403834522" MODIFIED="1712403838794"/>
+</node>
+</node>
+<node TEXT="rebuttal" POSITION="right" ID="ID_1075423546" CREATED="1712401908774" MODIFIED="1712403196711">
+<edge COLOR="#ff0000"/>
+<font BOLD="true"/>
+<node TEXT="inconsistent with TRIPs" ID="ID_1532272774" CREATED="1712402713482" MODIFIED="1712402728942"/>
+<node TEXT="unique trait" ID="ID_427786627" CREATED="1712402736942" MODIFIED="1712403922703"/>
+<node TEXT="suggestive" ID="ID_1432786902" CREATED="1712402748427" MODIFIED="1712402753352"/>
+</node>
+<node TEXT="resubmission" POSITION="right" ID="ID_1854336933" CREATED="1712402949671" MODIFIED="1714371670632">
+<edge COLOR="#007c7c"/>
+<font BOLD="true"/>
+<node TEXT="expert opinion" ID="ID_1040834188" CREATED="1712403043294" MODIFIED="1714371674326" VSHIFT_QUANTITY="0.75 pt">
+<node TEXT="specimens [TBA]" ID="ID_365552183" CREATED="1712618292211" MODIFIED="1714371684441" HGAP_QUANTITY="14.75 pt" VSHIFT_QUANTITY="1.5 pt">
+<node TEXT="lvl 5 CryptoWizard" ID="ID_1204716376" CREATED="1712619044659" MODIFIED="1714371678274" HGAP_QUANTITY="24.5 pt" VSHIFT_QUANTITY="-3.75 pt"/>
+<node TEXT="lvl 4 DAOsigner" ID="ID_145921374" CREATED="1712619073516" MODIFIED="1714371684439" HGAP_QUANTITY="25.25 pt" VSHIFT_QUANTITY="5.25 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="refusal" POSITION="left" ID="ID_456815905" CREATED="1712401916635" MODIFIED="1714371956298" HGAP_QUANTITY="18.5 pt" VSHIFT_QUANTITY="24.75 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="confusion" ID="ID_245886227" CREATED="1712401979504" MODIFIED="1712401987595"/>
+<node TEXT="generic" ID="ID_357545467" CREATED="1712401988371" MODIFIED="1712402010845">
+<node TEXT="non-descriptive" ID="ID_772557914" CREATED="1712402017420" MODIFIED="1712402533827"/>
+<node TEXT="deceptive" ID="ID_452697831" CREATED="1712402040774" MODIFIED="1712402045075"/>
+</node>
+<node TEXT="name" ID="ID_1005812033" CREATED="1712401973224" MODIFIED="1714371938088" HGAP_QUANTITY="17.75 pt" VSHIFT_QUANTITY="7.5 pt">
+<node TEXT="geographical" ID="ID_641101046" CREATED="1712402119401" MODIFIED="1712402126164"/>
+<node TEXT="cognomen" ID="ID_1544169667" CREATED="1712402126879" MODIFIED="1712403404450"/>
+</node>
+<node TEXT="other" ID="ID_1925523767" CREATED="1712402420618" MODIFIED="1712402422657">
+<node TEXT="functional" ID="ID_1610822185" CREATED="1712402197062" MODIFIED="1712402202291"/>
+<node TEXT="multiple" ID="ID_26143580" CREATED="1712402208182" MODIFIED="1712402220473"/>
+<node TEXT="protected" ID="ID_1862522957" CREATED="1712402239918" MODIFIED="1712402250410"/>
+</node>
+<node TEXT="not trade/service" ID="ID_1764276162" CREATED="1712402273950" MODIFIED="1712402288610"/>
+</node>
+<node TEXT="Domain L&apos;Expert" POSITION="left" ID="ID_594185791" CREATED="1712618004417" MODIFIED="1714371964417" HGAP_QUANTITY="-431.49999 pt" VSHIFT_QUANTITY="-8.25 pt">
+<edge COLOR="#ffffff"/>
+<node TEXT="Subject L&apos;Expert&#xa;= [ co-author credit ]" ID="ID_386064849" CREATED="1712403092504" MODIFIED="1714371972357" HGAP_QUANTITY="19.25 pt" VSHIFT_QUANTITY="-6.75 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="Public as beneficiary ! customer" ID="ID_1644472589" CREATED="1712618488228" MODIFIED="1714371972355" HGAP_QUANTITY="-44.5 pt" VSHIFT_QUANTITY="-165 pt" NodeVisibility="HIDDEN">
+<hook NAME="FreeNode"/>
+<edge COLOR="#ffffff"/>
+</node>
+<node TEXT="Amorphous Org" ID="ID_1131516874" CREATED="1712619450904" MODIFIED="1714371457964" HGAP_QUANTITY="11 pt" VSHIFT_QUANTITY="44.25 pt"/>
+<node TEXT="Argument of Lawyers" ID="ID_1947773373" CREATED="1712619772464" MODIFIED="1714371453388" HGAP_QUANTITY="11.75 pt" VSHIFT_QUANTITY="-22.5 pt"/>
+<node TEXT="Squabs of Legal Engineers" ID="ID_242736999" CREATED="1712619653679" MODIFIED="1714371465041" HGAP_QUANTITY="13.25 pt" VSHIFT_QUANTITY="-9.75 pt"/>
+<node TEXT="Foreign Language" ID="ID_1944530689" CREATED="1712618877452" MODIFIED="1714371474741" HGAP_QUANTITY="11 pt" VSHIFT_QUANTITY="-1.5 pt"/>
+</node>
+</node>
+</node>
+</map>
